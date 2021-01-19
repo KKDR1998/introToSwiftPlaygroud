1a472f78672bb720b600304b2787eadf6bc81593
KKDR1998 <77356024+KKDR1998@users.noreply.github.com>
2021-01-19 10:39:26 -1000

    SP#1 Part 1 - 8 complete
    Class 3 Tuesday January 19

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,36 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//declare a variable, name it myFirstVariable and type it as a String
+//var myFirstVariable: String
+// String is a data type that is represented by a series of characters. The String must be surrounded by ""
+
+//assign myFirstVariable to a String
+var myFirstVariable = "I've created my first String, "
+
+//String Concatenation - combining Strings together
+
+// declare and assign a second variable all in one line
+var mySecondVariable = "and look I combined 2 Strings together."
+
+//create a third variable to hold the String concatenation
+
+var myThirdVariable = myFirstVariable + mySecondVariable
+
+print(myThirdVariable)
+
+//create three new variables
+var myNewVariableOne = "This new sentence is contradictory: "
+
+var myNewVariableTwo = "The next sentence is a lie. "
+
+var myNewVariableThree = "The previous sentence is the truth."
+
+//New String concatenation
+var myNewVariableFour = myNewVariableOne + myNewVariableTwo + myNewVariableThree
+
+//print new fourth variable
+print(myNewVariableFour)
+
+// If I want spaces to appear in the message, if I have multiple sentences, I just have to put a space at the end of the sentence "I read books.", instead I do "I read books. "
